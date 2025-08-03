787be219f721c80f637f00b9a8e9f05cdc72d0df
Marvin Friede <51965259+marvinfriede@users.noreply.github.com>
2025-08-03 18:23:05 +0200

    Bump version to 0.5.3 (#119)

--- src/tad_mctc/_version.py
+++ src/tad_mctc/_version.py
@@ -57,7 +57,7 @@ def version_tuple(version_string: str) -> tuple[int, ...]:
     return tuple(version_numbers)
 
 
-__version__ = "0.5.2"
+__version__ = "0.5.3"
 """Version of tad-mctc in semantic versioning."""
 
 __tversion__ = version_tuple(torch.__version__)
